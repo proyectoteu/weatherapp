b1e4a6d6efd09662a5f9d4a9edb82a967a8b0b03
teu <proyectoteu@gmail.com>
2018-06-25 14:40:56 -0500

    primer commit

---
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const WeatherExtraInfo = ({ humidity, wind }) => (
+    <div>
+        <span>{`${humidity}% - ` }</span>
+        <span>{`${wind} wind`}</span>
+    </div>
+);
+
+WeatherExtraInfo.prototype = {
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+}
+export default WeatherExtraInfo;
---
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import { SNOW } from './../../../constants/weathers';
+
+const WeatherData = () => (
+    <div>
+        <WeatherTemperature temperature={20} weatherState={SNOW}/>
+        <WeatherExtraInfo humidity={80} wind={'10m/s'}/>
+    </div>
+);
+
+export default WeatherData;
